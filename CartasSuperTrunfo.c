e62eccaa2812c5599ff225c0d3eef435266065d1
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-04-06 18:09:37 +0000

    teste com printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,7 +8,7 @@
 
 int main() {
     //teste
-    pritf("Super Trunfo");
+    printf("Super Trunfo\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
