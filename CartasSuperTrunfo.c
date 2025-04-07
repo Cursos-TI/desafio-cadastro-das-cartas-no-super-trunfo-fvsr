c849b2821422b5e493226f2251fa6e1fdd79a07c
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-04-07 16:09:48 +0000

    Comentários 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,13 +7,12 @@
 //Teste larissa
 
 int main() {
-    //teste
-    printf("Super Trunfo\n");
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
 
     //Declaração de váriaveis para carta 1
-    
+
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
