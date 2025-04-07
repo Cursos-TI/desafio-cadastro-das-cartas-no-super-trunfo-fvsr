8613878a51473c2de7d88385f54ac039c2c4ceb7
Fábio Soriano <fabio_vsr@yahoo.com.br>
2025-04-07 16:08:17 +0000

    Comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,9 @@ int main() {
     printf("Super Trunfo\n");
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+
+    //Declaração de váriaveis para carta 1
+    
     
     // Cadastro das Cartas:
     // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
